b46ed1fba7489477227b82acbc58dbbfca39e03f
nmiesmer <nic.miesmer@segmint.com>
2018-10-30 23:13:38 -0400

    Submit button works but the modal doesn't

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,14 +18,15 @@ router.post('/api/friends', function(req, res){
      var scoreDifferenceArray = [];
      var bestFriendMatch = 0;
      
-     //loop to run through the current friends list
-     for (var i=0; i<friends.length; i++){
+     //loop through 
+     for (var i=0; i<friendsData.length; i++){
          var difference = 0;
-         //run through the scores in the friends array to compare friends
+         
+         //run through the scores in the friends array, and sum the absolute difference
          for (var j=0; j<surveyAnswers.length; j++){            
-             difference += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(surveyAnswers[j])));
+             difference += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(surveyAnswers[j])));
          }
-         //push into the scoresArray
+         //push into the score difference array to compare later
          scoreDifferenceArray.push(difference);
      }
      // after all friends are compared find the best match based on similar scores
@@ -34,11 +35,10 @@ router.post('/api/friends', function(req, res){
             bestFriendMatch = k;
          }
      }
-     //retun bestMatch data
-     var foundFriend = friends[bestFriendMatch];
+     var foundFriend = friendsData[bestFriendMatch];
+
      //push new submission into the friends array
-     
-     friends.push(req.body);
+     friendsData.push(req.body);
      res.json(foundFriend);
      
  });
